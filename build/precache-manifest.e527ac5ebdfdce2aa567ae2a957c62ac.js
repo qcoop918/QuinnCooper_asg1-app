a13dc63b701572f4d848e2745eb785eadb8f9610
qcoop918 <47125657+qcoop918@users.noreply.github.com>
2020-03-03 12:37:38 -0700

    Add files via upload

---
+++ build/precache-manifest.e527ac5ebdfdce2aa567ae2a957c62ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78ee4514fd097bbe1fc4cdfa623c160",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d94d47d44e6a4cf84da",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "136ad76f025dcdaff7c7",
+    "url": "/static/js/2.0783c941.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.0783c941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d94d47d44e6a4cf84da",
+    "url": "/static/js/main.c04ea408.chunk.js"
+  },
+  {
+    "revision": "aaf3ef48e9cd971ddf96",
+    "url": "/static/js/runtime-main.47fd172b.js"
+  }
+]);
